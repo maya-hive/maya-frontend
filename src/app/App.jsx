4a80a3429f82b86ee4437ad2c74ebd783df796d7
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-05-06 23:41:10 +0530

    fix fonts not loading on production bug

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import { CustomCursor, Layout } from '@components';
 import { CursorContextProvider, ThemeContextProvider } from '@contexts';
 import { clients, projects, testimonials, theme } from '@services';
 import { fetchData } from '@helpers';
+import Head from 'next/head';
 
 export const App = ({
 	Component,
@@ -13,6 +14,18 @@ export const App = ({
 }) => (
 	<ThemeContextProvider themeData={themeData}>
 		<CursorContextProvider>
+			<Head>
+				{/* eslint-disable-next-line @next/next/no-page-custom-font */}
+				<link
+					href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+					rel="stylesheet"
+				/>
+				{/* eslint-disable-next-line @next/next/no-page-custom-font */}
+				<link
+					href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+					rel="stylesheet"
+				/>
+			</Head>
 			<CustomCursor />
 			<Layout>
 				<Component
