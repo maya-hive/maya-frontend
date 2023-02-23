0a8b3ef5823a1676528bee5ec77522075a080502
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2023-02-23 17:13:41 +0530

    update: add client side validation to forms, add prod & staging env to workflows

--- src/components/form/PopupForm.jsx
+++ src/components/form/PopupForm.jsx
@@ -1,7 +1,10 @@
 import withReactContent from 'sweetalert2-react-content';
 import { CloseButton, Modal } from 'react-bootstrap';
 import { useEffect, useRef, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import Swal from 'sweetalert2';
+import { z } from 'zod';
 
 import styles from './PopupForm.module.scss';
 import { Submit } from '@components';
@@ -21,9 +24,7 @@ export const PopupForm = ({
 	const [isLoading, setIsLoading] = useState(false);
 	const [isActive, setIsActive] = useState(false);
 	const [primaryColor, setColor] = useState('');
-	const [formData, setFormData] = useState({});
 
-	const formRef = useRef(null);
 	const fileLabelRef = useRef(null);
 	const fileInputRef = useRef(null);
 
@@ -31,20 +32,34 @@ export const PopupForm = ({
 
 	const delay = 2_000;
 
-	const handleInput = e => {
-		e.preventDefault();
-		setFormData({
-			...formData,
-			[e.target.name]: e.target.value,
-			context: headline,
-		});
+	const schema = z.object({
+		name: z.string().min(2).max(255),
+		email: z.string().email().min(2).max(255),
+		message: z.string().min(2).max(255),
+		phone: z.string().min(2).max(255),
+		context: z.string().min(2).max(255),
+		file: fileInput ? z.string() : z.any(),
+	});
+
+	const {
+		reset,
+		register,
+		setValue,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: zodResolver(schema),
+		mode: 'onBlur',
+	});
+
+	const handleButtonOnHide = e => {
+		setIsPoppedData({});
+		setIsActive(false);
 	};
 
-	const handleFileInput = e => {
-		e.preventDefault();
-
-		let file = e.target.files[0];
-		let fileReader = new FileReader();
+	const handleFileOnChange = event => {
+		const fileReader = new FileReader();
+		let file = event.target.files[0];
 
 		if (file) {
 			fileReader.readAsDataURL(file);
@@ -57,20 +72,11 @@ export const PopupForm = ({
 		}
 
 		fileReader.onload = event => {
-			setFormData({
-				...formData,
-				[e.target.name]: event.target.result,
-			});
+			setValue('file', event.target.result, { shouldValidate: true });
 		};
 	};
 
-	const handleButtonOnHide = e => {
-		setIsPoppedData({});
-		setIsActive(false);
-	};
-
-	const handleFormSubmit = e => {
-		e.preventDefault();
+	const onSubmit = data => {
 		setIsLoading(true);
 
 		MySwal.fire({
@@ -88,7 +94,7 @@ export const PopupForm = ({
 				Accept: 'application/json',
 				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify({ ...formData }),
+			body: JSON.stringify({ ...data }),
 		})
 			.then(res => res.json())
 			.then(res => {
@@ -102,7 +108,7 @@ export const PopupForm = ({
 						timer: 3000,
 					});
 					if (res.type === 'success') {
-						setFormData({});
+						reset();
 						setIsActive(false);
 						setIsPoppedData({});
 					}
@@ -111,7 +117,6 @@ export const PopupForm = ({
 			.catch(({ errors, type }) => {
 				setTimeout(() => {
 					setIsLoading(false);
-					setFormData({});
 					MySwal.fire({
 						title: errors ? errors[0] : 'Something Went Wrong',
 						icon: type,
@@ -129,13 +134,14 @@ export const PopupForm = ({
 	}, [primaryColor]);
 
 	useEffect(() => {
+		setValue('context', headline);
 		headline.replace(/\s/g, '') === Object.keys(isPoppedData)[0] &&
 			setIsActive(true);
-	}, [headline, isPoppedData]);
+	}, [headline, isPoppedData, setValue]);
 
 	return (
 		<Modal centered show={isActive} onHide={handleButtonOnHide}>
-			<form ref={formRef} className={styles.main}>
+			<form className={styles.main} onSubmit={handleSubmit(onSubmit)}>
 				<CloseButton
 					className={styles.closeBtn}
 					variant={'white'}
@@ -145,66 +151,70 @@ export const PopupForm = ({
 				<span className={styles.fieldGroup}>
 					<label htmlFor={'name'}>{popupData.name_label}</label>
 					<input
-						required
-						id={'name'}
-						name={'name'}
 						type={'text'}
 						placeholder={popupData.name_placeholder}
-						onInput={e => handleInput(e)}
+						{...register('name', { required: true })}
 					/>
+					{errors.name && (
+						<span className={styles.error}>Name is required</span>
+					)}
 				</span>
 				<span className={styles.fieldGroup}>
 					<label htmlFor={'email'}>{popupData.email_label}</label>
 					<input
 						required
-						id={'email'}
-						name={'email'}
 						type={'email'}
 						placeholder={popupData.email_placeholder}
-						onInput={e => handleInput(e)}
+						{...register('email', { required: true })}
 					/>
+					{errors.email && (
+						<span className={styles.error}>Email is required</span>
+					)}
 				</span>
 				<span className={styles.fieldGroup}>
 					<label htmlFor={'phone'}>{popupData.phone_label}</label>
 					<input
-						id={'phone'}
 						type={'tel'}
-						name={'phone'}
 						pattern={null}
-						placeholder={'0700 600 500'}
-						onInput={e => handleInput(e)}
+						placeholder={popupData.phone_placeholder}
+						{...register('phone', { required: true })}
 					/>
+					{errors.phone && (
+						<span className={styles.error}>Phone is required</span>
+					)}
 				</span>
 				{fileInput && (
 					<span className={`${styles.fieldGroup} ${styles.file}`}>
-						<label htmlFor={'file'} ref={fileLabelRef}>
+						<label
+							htmlFor={'file'}
+							{...register('file', { required: true })}
+							ref={fileLabelRef}
+							onClick={() => fileInputRef.current.click()}>
 							{popupData.file_label}
 						</label>
 						<input
-							required
-							id={'file'}
 							type={'file'}
-							name={'file'}
-							ref={fileInputRef}
 							accept={'application/pdf'}
-							onInput={e => handleFileInput(e)}
+							onChange={handleFileOnChange}
+							ref={fileInputRef}
 						/>
+						{errors.file && (
+							<span className={styles.error}>CV is required</span>
+						)}
 					</span>
 				)}
 				<span className={styles.fieldGroup}>
 					<label htmlFor={'message'}>{popupData.message_label}</label>
 					<textarea
-						id={'message'}
-						name={'message'}
 						placeholder={popupData.message_placeholder}
 						rows={'4'}
-						onInput={e => handleInput(e)}
+						{...register('message', { required: true })}
 					/>
+					{errors.message && (
+						<span className={styles.error}>Message is required</span>
+					)}
 				</span>
-				<Submit
-					className={styles.button}
-					isLoading={isLoading}
-					onClick={e => handleFormSubmit(e)}>
+				<Submit className={styles.button} isLoading={isLoading}>
 					{popupData.submit}
 				</Submit>
 			</form>
