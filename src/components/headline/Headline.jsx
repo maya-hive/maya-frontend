33094dd3810bfbd769ee0d0f99ba0131b95e156b
BlazeIsClone <sandevabeykoon123@gmail.com>
2023-08-21 17:31:45 +0530

    fix: front pages remove duplicate h1 tags

--- src/components/headline/Headline.jsx
+++ src/components/headline/Headline.jsx
@@ -1,7 +1,23 @@
 import styles from './Headline.module.scss';
 
-export const Headline = ({ styles: propStyles, children }) => (
-	<h2 className={`${styles.main} ${propStyles || ''}`}>
-		<span dangerouslySetInnerHTML={{ __html: children }} />
-	</h2>
-);
+export const Headline = ({ styles: propStyles, children, h1, div }) => {
+	if (h1) {
+		return (
+			<h1 className={`${styles.main} ${propStyles || ''}`}>
+				<span dangerouslySetInnerHTML={{ __html: children }} />
+			</h1>
+		);
+	} else if (div) {
+		return (
+			<div className={`${styles.main} ${propStyles || ''}`}>
+				<span dangerouslySetInnerHTML={{ __html: children }} />
+			</div>
+		);
+	}
+
+	return (
+		<h2 className={`${styles.main} ${propStyles || ''}`}>
+			<span dangerouslySetInnerHTML={{ __html: children }} />
+		</h2>
+	);
+};
